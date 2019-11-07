7abd28b77b229dddb5b71d2765e5ca8ead0370d3
이규종 <kyujong93@naver.com>
2019-11-07 14:35:59 +0900

    mongodb connection altas

--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import { GraphQLServer } from 'graphql-yoga';
+import morgan from 'morgan';
 import resolvers from './graphql/resolver';
 import mongoose from 'mongoose';
 
 const PORT = process.env.PORT || 4000;
 
 // mongoose connect
-mongoose.connect(
-  'mongodb+srv://user:user@cluster0-62rou.mongodb.net/test?retryWrites=true&w=majority',
-  { useNewUrlParser: true }
-);
-mongoose.connection.once('open', () => console.log('connected to database'));
+mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true });
+mongoose.connection.once('open', () => console.log('✅ connected to database'));
+
+import './models/authorSchema';
+import './models/bookSchema';
 
 const server = new GraphQLServer({
   typeDefs: 'src/graphql/schema.graphql',
   resolvers
 });
 
+server.express.use(morgan('dev'));
+
 server.start({ port: PORT }, () =>
-  console.log(`server running on port ${PORT}`)
+  console.log(`✅ server running on port ${PORT}`)
 );
